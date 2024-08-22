fcda28edabe7f424ca1bbdf3573eb2e084fef7d9
Vera869 <verea8910@gmail.com>
2024-08-23 00:05:11 +0700

    + hidden nav menu

--- src/components/header/HeaderBox.tsx
+++ src/components/header/HeaderBox.tsx
@@ -1,13 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { NavMenu } from "../navMenu/NavMenuBox";
 import "./HeaderBoxStyle.css";
 
 export const Header: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const home = location.pathname === "/";
 
   return <div className="header">
     <img className="header-icon" src="../../../src/assets/img/sale-2.png" alt="Home" onClick={() => navigate("/")}/>
     <h1>A Big Fashion Sale</h1>
-    <NavMenu />
+    {home ? <NavMenu /> : ""}
   </div>;
 };
