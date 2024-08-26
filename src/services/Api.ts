8438907bdf78d881ed098b614d27808c2282ef7e
Vera869 <verea8910@gmail.com>
2024-08-26 16:32:11 +0700

    + typified async functions

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,21 +1,23 @@
+import { Item } from "../Types";
+
 export const host = "https://fakestoreapi.com/products/";
 
-export async function getAllProducts(category: string){
+export async function getAllProducts(category: string): Promise<Item[]>{
   const response = await fetch(`${host}category/${category}`);
   if(!response.ok) {
     throw new Error("Произошла ошибка");
   } 
-  const data = response.json();
+  const data: Promise<Item[]> = response.json();
 
   return data;
 }
 
-export async function getProductById(id: string| undefined){
+export async function getProductById(id: string| undefined): Promise<Item>{
   const response = await fetch(`${host}${id}`);
   if(!response.ok) {
     throw new Error("Произошла ошибка");
   } 
-  const data = response.json();
+  const data: Promise<Item> = response.json();
  
   return data;
 }
